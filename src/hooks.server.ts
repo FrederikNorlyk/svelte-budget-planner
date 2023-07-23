d8baa22342917ec153bf8481ea3e54305804c0a6
Frederik <132744263+FrederikNorlyk@users.noreply.github.com>
2023-07-23 12:29:56 +0000

    eslint

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,9 +3,6 @@ import GitHub from "@auth/core/providers/github";
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
 import { redirect, type Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
-import type { Account, Profile, Session } from "@auth/core/types";
-import type { JWT } from "@auth/core/jwt";
-import type { AdapterUser } from "@auth/core/adapters";
 
 type HandleParams = Parameters<Handle>[0];
 
@@ -13,7 +10,6 @@ async function authorization({ event, resolve }: HandleParams) {
   const session = await event.locals.getSession();
   const isLoggedIn = session;
 
-  // Protect any routes under /authenticated
   if (event.url.pathname.startsWith("/auth") && isLoggedIn) {
     throw redirect(303, "/");
   }
@@ -22,18 +18,14 @@ async function authorization({ event, resolve }: HandleParams) {
     throw redirect(303, "/auth");
   }
 
-  // If the request is still here, just proceed as normally
   return resolve(event);
 }
 
-// First handle authentication, then authorization
-// Each function acts as a middleware, receiving the request handle
-// And returning a handle which gets passed to the next function
 export const handle: Handle = sequence(
   SvelteKitAuth({
     providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
     callbacks: {
-      async session({session, token}) {
+      async session({ session, token }) {
         return Promise.resolve({ ...session, user: { ...session.user, id: token.sub } })
       }
     }
