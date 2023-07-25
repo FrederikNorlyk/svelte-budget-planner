0a6f4b700039888d2f0d22a5742d8dd696f1fe68
Frederik <132744263+FrederikNorlyk@users.noreply.github.com>
2023-07-25 15:53:26 +0000

    Added icons

---
+++ src/lib/icons/Icon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let size: number;
+	export let cssClass: string | undefined;
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	class="h-{size} w-{size} {cssClass ?? ''}"
+>
+	<slot />
+</svg>
---
+++ src/lib/icons/IconArrowCircleRight.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Icon from "./Icon.svelte";
+
+	export let size: number;
+	export let cssClass: string | undefined;
+</script>
+
+<Icon {size} {cssClass}>
+	<path
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+	/>
+</Icon>
