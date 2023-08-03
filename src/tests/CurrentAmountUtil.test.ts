ef46e30f3707806d64c8f82346387aea898e424e
Frederik <132744263+FrederikNorlyk@users.noreply.github.com>
2023-08-03 15:44:17 +0000

    Added vitest

---
+++ src/tests/CurrentAmountUtil.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest'
+import { CurrentAmountUtil } from '$lib/util/CurrentAmountUtil';
+import { Account } from '$lib/models/Account';
+import { Expense } from '$lib/models/Expense';
+
+test('Monthly expenses require no payment date entries', () => {
+    const account = new Account(1, "Test");
+    
+    account.setExpenses([
+        new Expense(1, "Test", 123, 1, "Test", 1, true)
+    ]);
+
+    const amount = CurrentAmountUtil.getCurrentAmmount(account)
+    expect(amount).toBe(123)
+})
