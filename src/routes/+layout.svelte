1c623ac2064de20fcaa2832d369434982fe17b13
Frederik <132744263+FrederikNorlyk@users.noreply.github.com>
2023-08-05 09:23:15 +0000

    Added more page margin for large screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,11 +39,11 @@
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar shadow="shadow-md">
+		<AppBar shadow="shadow-md" padding="p-3 pl-5 pr-5 sm:pl-16 sm:pr-16 md:pl-20 md:pr-20 2xl:pl-36 2xl:pr-36">
 			<svelte:fragment slot="lead">
 				<a
 					href="/accounts"
-					class="ml-5 select-none text-3xl sm:ml-16"
+					class="select-none text-3xl"
 					style="font-family: 'Permanent Marker', cursive;"
 				>
 					Budget planner
@@ -100,7 +100,7 @@
 		</AppBar>
 	</svelte:fragment>
 
-	<div class="m-5 sm:ml-16 sm:mr-16 sm:mt-10 md:ml-20 md:mr-20">
+	<div class="m-5 sm:ml-16 sm:mr-16 sm:mt-10 md:ml-20 md:mr-20 2xl:ml-36 2xl:mr-36">
 		<slot />
 	</div>
 </AppShell>
