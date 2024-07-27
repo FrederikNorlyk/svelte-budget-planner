984f5c72a686851322feda8ab468cd79147d5379
Frederik Norlyk <frederiknorlyk@gmail.com>
2024-07-27 07:11:36 +0200

    Styling of header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,34 +1,88 @@
 <script lang="ts">
 	import IconArrowLeft from '$lib/icons/IconArrowLeft.svelte';
+	import IconBarsThree from '$lib/icons/IconBarsThree.svelte';
 	import IconPencilSquare from '$lib/icons/IconPencilSquare.svelte';
 	import { i18n, type ReplacementVariables } from '$lib/localization/i18n';
+	import {
+		Toast,
+		Modal,
+		type PopupSettings,
+		popup,
+		initializeStores
+	} from '@skeletonlabs/skeleton';
 
 	export let title: string;
 	export let titleParams: ReplacementVariables | undefined;
 	export let details: string;
 	export let backHref: string | undefined = undefined;
 	export let editHref: string | undefined = undefined;
+	export let userProfilePicture: string;
+
+	const userMenuPopup: PopupSettings = {
+		event: 'click',
+		target: 'userMenuPopup',
+		placement: 'bottom'
+	};
 </script>
 
 <div class="flex space-x-2">
-	{#if backHref}
-		<a class="mt-2" href={backHref}>
-			<button type="button" class="variant-filled btn-icon" aria-label="Back button">
-				<IconArrowLeft cssClass="h-8 w-8" />
-			</button>
-		</a>
-	{/if}
-
-	<div>
-		{#if editHref}
-			<a class="flex space-x-1" href={editHref} aria-label="Edit">
-				<h1 class="text-3xl">{$i18n(title, titleParams)}</h1>
-				<IconPencilSquare cssClass="flex-none h-5 w-5" />
+	<div class="flex-grow">
+		{#if backHref}
+			<a class="mt-2" href={backHref}>
+				<button type="button" class="variant-filled btn-icon" aria-label="Back button">
+					<IconArrowLeft cssClass="h-8 w-8" />
+				</button>
 			</a>
-			<p>{$i18n(details)}</p>
-		{:else}
-			<h1 class="text-3xl">{$i18n(title, titleParams)}</h1>
-			<p>{$i18n(details)}</p>
 		{/if}
+
+		<div>
+			{#if editHref}
+				<a class="flex space-x-1" href={editHref} aria-label="Edit">
+					<h1 class="text-3xl">{$i18n(title, titleParams)}</h1>
+					<IconPencilSquare cssClass="flex-none h-5 w-5" />
+				</a>
+				<p>{$i18n(details)}</p>
+			{:else}
+				<h1 class="text-3xl">{$i18n(title, titleParams)}</h1>
+				<p>{$i18n(details)}</p>
+			{/if}
+		</div>
+	</div>
+
+	<a class="variant-ghost-surface btn btn-sm hidden h-8 md:block" href="/accounts" rel="noreferrer"
+		>{$i18n('accounts.title')}</a
+	>
+	<a class="variant-ghost-surface btn btn-sm hidden h-8 md:block" href="/balance" rel="noreferrer"
+		>{$i18n('currentAmount.title')}</a
+	>
+
+	<a href="/settings" class="hidden md:block">
+		<img
+			src={userProfilePicture}
+			alt="Current user"
+			class="h-8 w-8 overflow-hidden rounded-full object-cover"
+		/>
+	</a>
+
+	<button
+		class="variant-filled btn-icon h-10 w-12 md:hidden"
+		use:popup={userMenuPopup}
+		aria-label="Menu button"
+	>
+		<IconBarsThree cssClass="w-8 h-8" />
+	</button>
+
+	<div data-popup="userMenuPopup">
+		<div class="borderborder-gray-400 card mr-3 mt-3 w-40 space-y-2 p-4 shadow-xl">
+			<a class="variant-ghost btn w-full" href="/accounts" rel="noreferrer"
+				>{$i18n('accounts.title')}</a
+			>
+			<a class="variant-ghost btn w-full" href="/balance" rel="noreferrer"
+				>{$i18n('currentAmount.title')}</a
+			>
+			<a class="variant-ghost btn w-full" href="/settings" rel="noreferrer"
+				>{$i18n('settings.title')}</a
+			>
+		</div>
 	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,111 +1,37 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { i18n } from '$lib/localization/i18n';
 	import { page } from '$app/stores';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-
-	import {
-		AppShell,
-		AppBar,
-		Toast,
-		Modal,
-		type PopupSettings,
-		popup,
-		initializeStores
-	} from '@skeletonlabs/skeleton';
-	import IconBarsThree from '$lib/icons/IconBarsThree.svelte';
+	import { Toast, Modal, initializeStores } from '@skeletonlabs/skeleton';
 	import Header from '$lib/components/Header.svelte';
 
 	initializeStores();
 
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
-	const userMenuPopup: PopupSettings = {
-		event: 'click',
-		target: 'userMenuPopup',
-		placement: 'bottom'
-	};
-
 	const user = $page.data.session!.user;
 
-	function getProfilePicture() {
-		return user.image ?? `https://avatars.dicebear.com/api/identicon/${user.id ?? 'unknown'}.svg`;
-	}
+	const profilePicture =
+		user.image ?? `https://avatars.dicebear.com/api/identicon/${user.id ?? 'unknown'}.svg`;
 </script>
 
 <Modal />
 <Toast />
 
-<AppShell>
-	<svelte:fragment slot="header">
-		<AppBar
-			shadow="shadow-md"
-			padding="p-3 pl-5 pr-5 sm:pl-16 sm:pr-16 md:pl-20 md:pr-20 2xl:pl-36 2xl:pr-36"
-		>
-			<svelte:fragment slot="lead">
-				<a
-					href="/accounts"
-					class="select-none text-3xl"
-					style="font-family: 'Permanent Marker', cursive;"
-				>
-					Budget planner
-				</a>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="variant-ghost-surface btn btn-sm hidden sm:block"
-					href="/accounts"
-					rel="noreferrer">{$i18n('accounts.title')}</a
-				>
-				<a class="variant-ghost-surface btn btn-sm hidden sm:block" href="/balance" rel="noreferrer"
-					>{$i18n('currentAmount.title')}</a
-				>
-
-				<a href="/settings" class="hidden sm:block">
-					<img
-						src={getProfilePicture()}
-						alt="Current user"
-						class="h-12 w-12 overflow-hidden rounded-full object-cover"
-					/>
-				</a>
-
-				<button class="pr-4 sm:hidden" use:popup={userMenuPopup} aria-label="Menu button">
-					<IconBarsThree cssClass="w-8 h-8" />
-				</button>
-
-				<div data-popup="userMenuPopup">
-					<div class="borderborder-gray-400 card mr-3 mt-3 w-40 space-y-2 p-4 shadow-xl">
-						<a class="variant-ghost btn w-full sm:hidden" href="/accounts" rel="noreferrer"
-							>{$i18n('accounts.title')}</a
-						>
-						<a class="variant-ghost btn w-full sm:hidden" href="/balance" rel="noreferrer"
-							>{$i18n('currentAmount.title')}</a
-						>
-						<a class="variant-ghost btn w-full sm:hidden" href="/settings" rel="noreferrer"
-							>{$i18n('settings.title')}</a
-						>
-					</div>
-				</div>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-
-	<div class="m-5 sm:ml-16 sm:mr-16 sm:mt-10 md:ml-20 md:mr-20 2xl:ml-36 2xl:mr-36">
+<main>
+	<div class="m-5 sm:ml-16 sm:mr-16 md:ml-20 md:mr-20 2xl:ml-36 2xl:mr-36">
 		<Header
 			title={$page.data.title}
 			titleParams={$page.data.titleParams}
 			details={$page.data.details}
 			backHref={$page.data.backHref}
 			editHref={$page.data.editHref}
+			userProfilePicture={profilePicture}
 		/>
 
 		<div class="mt-3">
 			<slot />
 		</div>
 	</div>
-</AppShell>
-
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');
-</style>
+</main>
