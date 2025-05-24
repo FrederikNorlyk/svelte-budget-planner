895f33040a34c030ec333c7586b931d4bc9935f1
FrederikNorlyk <frederiknorlyk@gmail.com>
2025-05-24 12:39:57 +0200

    Changed SelectField.svelte to only accept specific types for its values.

--- src/lib/components/SelectField.svelte
+++ src/lib/components/SelectField.svelte
@@ -1,4 +1,4 @@
-<script lang="ts" generics="T extends object">
+<script lang="ts" generics="T extends string | number | boolean">
 	import type { SelectOption } from './types/SelectOption';
 
 	interface Props {
