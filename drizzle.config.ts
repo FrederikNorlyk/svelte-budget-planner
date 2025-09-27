2c158c7b8f373dfb88b3726615aba0eacb755cb8
FrederikNorlyk <frederiknorlyk@gmail.com>
2025-09-27 13:25:23 +0200

    Created a separate db for this project

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.POSTGRES_URL) throw new Error('POSTGRES_URL is not set');
+if (!process.env.NEON_DATABASE_URL) throw new Error('NEON_DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
-	dbCredentials: { url: process.env.POSTGRES_URL },
+	dbCredentials: { url: process.env.NEON_DATABASE_URL },
 	verbose: true,
 	strict: true
 });
