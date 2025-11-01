535aad51a12b7ce6527de43726a9e60009333805
FrederikNorlyk <frederiknorlyk@gmail.com>
2025-11-01 11:33:14 +0100

    Made AddButton support <a> ...rest props

--- src/lib/components/AddButton.svelte
+++ src/lib/components/AddButton.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
 	import Plus from 'lucide-svelte/icons/plus';
-	import type { ResolvedPathname } from '$app/types';
 
-	interface Props {
-		href: ResolvedPathname;
-		ariaLabel: string;
-	}
-
-	const { href, ariaLabel }: Props = $props();
+	const { ...rest } = $props();
 </script>
 
 <a
 	class="group card-primary clickable border-surface-200-800 hover:border-surface-300-700 flex items-center justify-center border-4 border-dashed p-10"
-	{href}
-	aria-label={ariaLabel}
+	{...rest}
 >
 	<Plus size="55" class="text-primary-400-600 group-hover:text-primary-600-400" />
 </a>
