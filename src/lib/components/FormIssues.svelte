c74901e2c4d7c87effbb64e2f6e7ac79cb72039c
FrederikNorlyk <frederiknorlyk@gmail.com>
2025-11-02 12:56:47 +0100

    Made FormIssues.svelte translate messages

--- src/lib/components/FormIssues.svelte
+++ src/lib/components/FormIssues.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { RemoteFormIssue } from '@sveltejs/kit';
+	import { _ } from 'svelte-i18n';
 
 	interface Props {
 		issues: RemoteFormIssue[] | undefined;
@@ -12,7 +13,7 @@
 	<ul class="card bg-error-100-900 p-4">
 		{#each issues as issue, index (index)}
 			<li class="text-error-contrast-100-900">
-				{issue.message}
+				{$_(issue.message)}
 			</li>
 		{/each}
 	</ul>
